2a8d2c69b57aeff5ae8dd89dfc64298b6688e7ca
Its-SheldonDev <marius.bzcn@gmail.com>
2024-06-28 14:48:49 +0200

    Modif Header 📰

--- includes/header.php
+++ includes/header.php
@@ -11,6 +11,7 @@
         <ul>
             <li class="logo"><a href="../index.php"><img src="../assets/img/logo.png" alt="QR Cabane Logo"></a></li>
             <li class="right">
+                <a href="https://linktr.ee/cabane.parias"><img class="icon" src="../assets/img/svg/link.svg" alt="Qr icon"> LINKTREE</a>
                 <a href="stats.php"><img class="icon" src="../assets/img/svg/stats.svg" alt="Stats icon"> STATS</a>
                 <a href="graph.php"><img class="icon" src="../assets/img/svg/graph.svg" alt="Graphs icon"> GRAPH</a>
                 <a href="logout.php"><img class="icon" src="../assets/img/svg/logout.svg" alt="Logout icon"> LOGOUT</a>
